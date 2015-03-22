bc43dcb6edbbd05adde016706983b48f364535c4
Karlos <karel.steinmetz@gmail.com>
2015-03-22 08:59:15 +0100

    Removed junit dependency and used kotlin.test.assertions

--- kotlin-fluent-assertions-core/src/main/kotlin/org/ks/kotlinfluentassertions/core/ShouldValueHolder.kt
+++ kotlin-fluent-assertions-core/src/main/kotlin/org/ks/kotlinfluentassertions/core/ShouldValueHolder.kt
@@ -1,10 +1,10 @@
 package org.ks.kotlinfluentassertions.core
 
-import org.junit.Assert.assertThat
-import org.hamcrest.CoreMatchers.equalTo
+import kotlin.test.assertEquals
+
 
 class ShouldValueHolder(var value: Any) {
     public fun be<T>(v: T) {
-        assertThat(value, equalTo(v: Any))
+        assertEquals(value, v)
     }
 }
